154a5005040035ab4093ac5c282581d6d8a6aa65
Vasiliy <57954863+VasiliyNichnikov@users.noreply.github.com>
2022-01-20 18:28:56 +0300

    Инициализирована БД

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir, "app.db")
+SQLALCHEMY_MIGRATE_REPO = os.path.join(base_dir, "db_repository")
+
+
 CSRF_ENABLED: bool = True
 SECRET_KEY: str = "Z0deICTEIc|i}ypdv{0zWKL95"
 
